1d39ef6c1c238708a1692cfcfbb62054cc81f760
unknown <vojtek.nowak@gmail.com>
2011-10-03 13:38:41 +0200

    Inicjacja projektu na githubie

---
+++ procent skladany/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar3 = new System.Windows.Forms.HScrollBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "wpłata stała";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "oprocentowanie roczne";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ilość lat";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ilość kapitalizacji w roku";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.AccessibleName = "Wpłata co kapitalizacje";
+            this.hScrollBar1.Location = new System.Drawing.Point(164, 19);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(106, 18);
+            this.hScrollBar1.TabIndex = 4;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // hScrollBar2
+            // 
+            this.hScrollBar2.LargeChange = 5;
+            this.hScrollBar2.Location = new System.Drawing.Point(164, 45);
+            this.hScrollBar2.Name = "hScrollBar2";
+            this.hScrollBar2.Size = new System.Drawing.Size(106, 18);
+            this.hScrollBar2.TabIndex = 5;
+            this.hScrollBar2.Value = 1;
+            this.hScrollBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar2_Scroll);
+            // 
+            // hScrollBar3
+            // 
+            this.hScrollBar3.LargeChange = 5;
+            this.hScrollBar3.Location = new System.Drawing.Point(164, 73);
+            this.hScrollBar3.Maximum = 300;
+            this.hScrollBar3.Name = "hScrollBar3";
+            this.hScrollBar3.Size = new System.Drawing.Size(106, 18);
+            this.hScrollBar3.TabIndex = 6;
+            this.hScrollBar3.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar3_Scroll);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(288, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(288, 49);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(288, 77);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(288, 112);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 11;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Location = new System.Drawing.Point(466, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(337, 105);
+            this.panel1.TabIndex = 12;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label10.Location = new System.Drawing.Point(40, 57);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 18);
+            this.label10.TabIndex = 1;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(16, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 24);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Wynik:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(334, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 107);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "oblicz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "4",
+            "12",
+            "365"});
+            this.comboBox1.Location = new System.Drawing.Point(164, 104);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(108, 21);
+            this.comboBox1.TabIndex = 14;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.zedGraphControl1.Location = new System.Drawing.Point(0, 145);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0;
+            this.zedGraphControl1.ScrollMaxX = 0;
+            this.zedGraphControl1.ScrollMaxY = 0;
+            this.zedGraphControl1.ScrollMaxY2 = 0;
+            this.zedGraphControl1.ScrollMinX = 0;
+            this.zedGraphControl1.ScrollMinY = 0;
+            this.zedGraphControl1.ScrollMinY2 = 0;
+            this.zedGraphControl1.Size = new System.Drawing.Size(831, 491);
+            this.zedGraphControl1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(831, 636);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.hScrollBar3);
+            this.Controls.Add(this.hScrollBar2);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Moc procentu składanego.";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar2;
+        private System.Windows.Forms.HScrollBar hScrollBar3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+
+    }
+}
+
---
+++ procent skladany/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public class ZmienneGlobalne
+        {
+            public static int wplata, lat, kapitalizacje;
+            public static double procent, oprocentowanie;
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            label5.Text = Convert.ToString(hScrollBar1.Value);
+            ZmienneGlobalne.wplata=hScrollBar1.Value;
+            
+        }
+
+        private void hScrollBar2_Scroll(object sender, ScrollEventArgs e)
+        {
+            label6.Text = Convert.ToString(hScrollBar2.Value*0.01);
+            ZmienneGlobalne.oprocentowanie=hScrollBar2.Value*0.01;
+            
+            
+        }
+
+        private void hScrollBar3_Scroll(object sender, ScrollEventArgs e)
+        {
+            label7.Text = Convert.ToString(hScrollBar3.Value);
+            ZmienneGlobalne.lat = hScrollBar3.Value;
+            
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label8.Text = Convert.ToString(comboBox1.SelectedItem);
+            ZmienneGlobalne.kapitalizacje =Convert.ToInt32(comboBox1.Text) ; 
+             
+        }
+
+        
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ZmienneGlobalne.procent=ZmienneGlobalne.oprocentowanie/ZmienneGlobalne.kapitalizacje;
+            label10.Text = Convert.ToString(ZmienneGlobalne.wplata*((Math.Pow(1+ZmienneGlobalne.procent,ZmienneGlobalne.lat*ZmienneGlobalne.kapitalizacje)-1)/ZmienneGlobalne.procent)*(1+ZmienneGlobalne.procent));
+            
+            CreateGraph(zedGraphControl1);
+            zedGraphControl1.Refresh();
+            
+        }
+
+
+       
+
+        // Respond to form 'Load' event
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Setup the graph
+
+            CreateGraph(zedGraphControl1);
+       
+        }
+
+        // Build the Chart
+
+        private void CreateGraph(ZedGraphControl zgc)
+        {
+            // get a reference to the GraphPane
+
+            GraphPane myPane = zgc.GraphPane;
+
+            // Set the Titles
+
+            myPane.Title.Text = "Wykres prezentujący wzrost kapitału";
+            myPane.XAxis.Title.Text = "oś lat";
+            myPane.YAxis.Title.Text = "oś kapitału w złotych";
+
+            // Make up some data arrays based on the Sine function
+
+            double x, y1;
+            PointPairList list1 = new PointPairList();
+            for (int i = 0; i <= 20; i++)
+            {
+                x = i ;
+                y1 = ZmienneGlobalne.wplata * ((Math.Pow(1 + ZmienneGlobalne.procent, x * ZmienneGlobalne.kapitalizacje) - 1) / ZmienneGlobalne.procent) * (1 + ZmienneGlobalne.procent);
+                list1.Add(x, y1);
+                x = i + 1 ;
+            }
+
+            // Generate a red curve with diamond
+
+            // symbols, and "Porsche" in the legend
+
+            LineItem myCurve = myPane.AddCurve("Twój kapitał po n lat",
+                  list1, Color.Red, SymbolType.Diamond);
+
+            // Tell ZedGraph to refigure the
+
+            // axes since the data have changed
+
+            zgc.AxisChange();
+        }
+
+
+        
+
+
+
+
+    }
+}
